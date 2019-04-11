aff8bbfbd0b0c91d54bce3e42068917621d2353f
GrisWoldDiablo <griswoldd@hotmail.com>
2019-04-11 12:36:15 -0400

    Initial push
    The whole project

---
+++ My First Particle Swarm/Assets/GameManager.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{ 
+    public static float omegaAverage;
+    public static GameObject bestPositionGO;
+    public static LineRenderer lr;
+    public static Transform objectToFindStat;
+    public static Text currentOmegaTextStatic;
+    public static Transform objectToFind;
+    private static List<Particle> particles;
+
+    public GameObject bestPositionGOPrefab;
+    public ParticlePosition swarmBest;
+    public GameObject particleGO;
+    public GameObject particleGOHimmelblau;
+    public InputField inputFieldSpawnQTY;
+    public InputField inputFieldMovementAmount;
+    public InputField inputFieldOmega;
+    public InputField inputFieldPhi;
+    public Text currentOmegaText;
+    private Gradient colorGradient1;
+    public Toggle ptype;
+
+    private void Start()
+    {
+        bestPositionGO = Instantiate(bestPositionGOPrefab, null);
+        objectToFindStat = objectToFind;
+        if (bestPositionGO != null)
+        {
+            bestPositionGO.name = "Best Position";
+            bestPositionGO.AddComponent<LineRenderer>();
+            lr = bestPositionGO.GetComponent<LineRenderer>();
+            lr.SetPosition(0, Particle.bestTeamPosition.position);
+            lr.SetPosition(1, Particle.bestTeamPosition.position + Vector3.down * 250);
+            lr.startColor = Color.blue;
+            lr.endColor = Color.yellow;
+        }
+        swarmBest = Particle.bestTeamPosition;
+        currentOmegaTextStatic = currentOmegaText;
+    }
+
+    public void UpdateParticles()
+    {
+        foreach (Particle particle in FindObjectsOfType<Particle>())
+        {
+            particle.UpdateBests();
+        }
+    }
+
+    public void MoveParticles()
+    {
+        if (int.TryParse(inputFieldMovementAmount.text, result: out int amountOfMoves))
+        {
+            foreach (Particle particle in FindObjectsOfType<Particle>())
+            {
+                particle.Move(amountOfMoves);
+            } 
+        }
+        else
+        {
+            foreach (Particle particle in FindObjectsOfType<Particle>())
+            {
+                inputFieldMovementAmount.text = "1";
+                particle.Move();
+            }
+        }
+    }
+
+    public void SpawnParticles()
+    {
+        if (int.TryParse(inputFieldSpawnQTY.text, result: out int qty))
+        {
+            for (int i = 0; i < qty; i++)
+            {
+                if (ptype.isOn)
+                {
+                    int x = Random.Range(0, 480);
+                    int z = Random.Range(0, 480);
+                    Vector3 position = new Vector3(x, particleGO.transform.position.y, z);
+                    Instantiate(particleGO, position, particleGO.transform.rotation, null); 
+                }
+                else
+                {
+
+                    int x = Random.Range(-6, 6);
+                    int z = Random.Range(-6, 6);
+                    Vector3 position = new Vector3(x, particleGOHimmelblau.transform.position.y, z);
+                    Instantiate(particleGOHimmelblau, position, particleGOHimmelblau.transform.rotation, null);
+                }
+            }
+        }
+        else
+        {
+            inputFieldSpawnQTY.text = "10";
+            SpawnParticles();
+        }
+        UpdateParticles();
+        SetOmegaPhi();
+        GetListOfParticles();
+    }
+
+    public void RandomizeParticleLocation()
+    {
+        Vector3 position;
+        Particle.bestTeamPosition = new ParticlePosition();
+
+        foreach (Particle particle in FindObjectsOfType<Particle>())
+        {
+            if (ptype.isOn)
+            {
+                Particle.bestTeamPosition.pType = ParticleType.Depth;
+                float x = Random.Range(0, 480);
+                float z = Random.Range(0, 480);
+                position = new Vector3(x, particleGO.transform.position.y, z);
+
+            }
+            else
+            {
+                Particle.bestTeamPosition.pType = ParticleType.Himmelblau;
+                float x = Random.Range(-6.0f, 6.0f);
+                float z = Random.Range(-6.0f, 6.0f);
+                position = new Vector3(x, particleGOHimmelblau.transform.position.y, z);
+            }
+            particle.transform.position = position;
+            particle.bestPersonalPosition.position = position;
+            
+        }
+        UpdateParticles();
+        SetOmegaPhi();
+        GetListOfParticles();
+    }
+
+    public void SetOmegaPhi()
+    {
+        if (int.TryParse(inputFieldOmega.text,result: out int omega))
+        {
+            foreach (Particle particle in FindObjectsOfType<Particle>())
+            {
+                particle.omega = omega;
+            }
+        }
+
+        if (int.TryParse(inputFieldPhi.text, result: out int phi))
+        {
+            foreach (Particle particle in FindObjectsOfType<Particle>())
+            {
+                particle.phi = phi;
+            }
+        }
+    }
+
+    public static void UpdateOmega()
+    {
+        omegaAverage = 0;
+        foreach (Particle particle in particles)
+        {
+            omegaAverage += particle.omega;
+        }
+        omegaAverage /= particles.Count;
+        currentOmegaTextStatic.text = $"Omega Average: {omegaAverage}";
+    }
+
+    public void GetListOfParticles()
+    {
+        particles = new List<Particle>();
+        foreach (Particle particle in FindObjectsOfType<Particle>())
+        {
+            particles.Add(particle);
+        }
+    }
+
+    public void DestroyAllParticles()
+    {
+        Particle[] particlesArr = FindObjectsOfType<Particle>();
+        for (int i = 0; i < particlesArr.Length; i++)
+        {
+            Destroy(particlesArr[i].gameObject);
+        }
+    }
+}// End Class
---
+++ My First Particle Swarm/Assets/Particle.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ParticleType
+{
+    Depth,
+    Himmelblau
+}
+[System.Serializable]
+public class ParticlePosition
+{
+    public Vector3 position;
+    public float functionValue;
+    public float FunctionValue { get => GetFunctionValue(); }
+    public ParticleType pType = ParticleType.Depth;
+
+    //public ParticlePosition(Vector3 position = new pos)
+    //{
+    //    this.position = position;
+    //}
+
+    private float GetFunctionValue()
+    {
+        if (pType == ParticleType.Depth)
+        {
+            if (Physics.Raycast(position, Vector3.down, hitInfo: out RaycastHit hit, 500))
+            {
+                functionValue = hit.distance;
+                return functionValue;
+            }
+            else
+            {
+                return float.MinValue;
+            } 
+        }
+        else
+        {
+            // f(x,y) = (x2 + y - 11)2 + (x + y2 - 7)2 [-6,6]
+            float x = position.x;
+            float y = position.z;
+            float x2 = Mathf.Pow(x, 2);
+            float y2 = Mathf.Pow(y, 2);
+            functionValue = Mathf.Pow((x2 + y - 11),2) + Mathf.Pow((x + y2 - 7),2);
+            return functionValue;
+        }
+    }
+
+    
+}
+
+public class Particle : MonoBehaviour
+{
+    
+    public static ParticlePosition bestTeamPosition = new ParticlePosition();
+    public ParticleType pType = ParticleType.Depth;
+
+    public Vector3 currentPosition;
+    //public Vector3 currentDirection;
+    public ParticlePosition bestPersonalPosition;
+    public Vector3 currentVelocity;
+    public Vector3 endDestination;
+    public Vector3 startDestination;
+    public float phi = 2.0f;
+    public float omega = 0.1f;
+    private float journeyLenght;
+    public float DistanceToFloor { get => GetDistanceToGround(); }
+
+    public void Awake()
+    {
+        currentPosition = this.transform.position;
+        int randomDirection = Random.Range(1, 4);
+        switch (randomDirection)
+        {
+            case 1:
+                currentVelocity = this.transform.forward;
+                break;
+            case 2:
+                currentVelocity = -this.transform.forward;
+                break;
+            case 3:
+                currentVelocity = this.transform.right;
+                break;
+            case 4:
+            default:
+                currentVelocity = -this.transform.right;
+                break;
+        }
+        bestTeamPosition.pType = pType;
+        bestPersonalPosition.pType = pType;
+        bestPersonalPosition.position = currentPosition;
+        if (pType == ParticleType.Depth)
+        {
+            if (bestPersonalPosition.FunctionValue > bestTeamPosition.FunctionValue)
+            {
+                bestTeamPosition.position = currentPosition;
+            } 
+        }
+        else
+        {
+            if (bestPersonalPosition.FunctionValue < bestTeamPosition.FunctionValue)
+            {
+                bestTeamPosition.position = currentPosition;
+            }
+        }
+    }
+
+    public void MoveUpdate()
+    {
+        omega *= 0.99f;
+        GameManager.UpdateOmega();
+        float rp, rg;
+        rp = Random.value;
+        rg = Random.value;
+        currentVelocity = omega * currentVelocity +
+                       (phi * rp) * (bestPersonalPosition.position - currentPosition) +
+                       (phi * rg) * (bestTeamPosition.position - currentPosition);
+        startDestination = currentPosition;
+        endDestination = currentPosition + currentVelocity;
+        if (pType == ParticleType.Depth)
+        {
+            endDestination.x = Mathf.Clamp(endDestination.x, 0, 480);
+            endDestination.z = Mathf.Clamp(endDestination.z, 0, 480); 
+        }
+        else
+        {
+            endDestination.x = Mathf.Clamp(endDestination.x, -6, 6);
+            endDestination.z = Mathf.Clamp(endDestination.z, -6, 6);
+        }
+        journeyLenght = Vector3.Distance(startDestination, endDestination);
+    }
+
+    public void Move(int amountOfMoves = 1)
+    {
+        StartCoroutine(MoveParticle(amountOfMoves));
+    }
+
+    IEnumerator MoveParticle(int amountOfMoves = 1)
+    {
+
+        for (int i = 0; i < amountOfMoves; i++)
+        {
+            float speed;
+            this.name = $"M:{amountOfMoves - i-1}";
+            MoveUpdate();
+            float startTime = Time.time;
+            if (pType == ParticleType.Depth)
+            {
+                speed = 500.0f; 
+            }
+            else
+            {
+                speed = 50.0f;
+            }
+
+            while (currentPosition != endDestination)
+            {
+                float distanceCovered = (Time.time - startTime) * speed;
+                float fracJourney = distanceCovered / journeyLenght;
+                currentPosition = Vector3.Lerp(startDestination, endDestination, fracJourney);
+                this.transform.position = currentPosition;
+                yield return null;
+            }
+            UpdateBests();
+        }
+    }
+
+    public void UpdateBests()
+    {
+        currentPosition = this.transform.position;
+        if (pType == ParticleType.Depth)
+        {
+            if (GetDistanceToGround() > bestPersonalPosition.FunctionValue)
+            {
+                bestPersonalPosition.position = currentPosition;
+            }
+
+            if (bestPersonalPosition.FunctionValue > bestTeamPosition.FunctionValue)
+            {
+                bestTeamPosition.position = bestPersonalPosition.position;
+                Debug.DrawRay(bestTeamPosition.position, Vector3.down * 500, Color.blue, 5.0f);
+                SetBestGO();
+            } 
+        }
+        else
+        {
+            if (GetDistanceToGround() < bestPersonalPosition.FunctionValue)
+            {
+                bestPersonalPosition.position = currentPosition;
+            }
+
+            if (bestPersonalPosition.FunctionValue < bestTeamPosition.FunctionValue)
+            {
+                bestTeamPosition.position = bestPersonalPosition.position;
+                Debug.DrawRay(bestTeamPosition.position, Vector3.down * 500, Color.blue, 5.0f);
+                SetBestGO();
+            }
+        }
+    }
+
+    private static void SetBestGO()
+    {
+        GameObject.Find("BestDepthText").GetComponent<Text>().text = $"Best Depth = {bestTeamPosition.FunctionValue}";
+        GameManager.lr.SetPosition(0, bestTeamPosition.position);
+        GameManager.lr.SetPosition(1, bestTeamPosition.position + Vector3.down * 250);
+        GameManager.bestPositionGO.transform.position = bestTeamPosition.position;
+    }
+
+    private float GetDistanceToGround()
+    {
+        if (pType == ParticleType.Depth)
+        {
+            Debug.DrawRay(currentPosition, Vector3.down * 500, Color.red, 0.5f);
+            if (Physics.Raycast(currentPosition, Vector3.down, hitInfo: out RaycastHit hit, 500))
+            {
+                return hit.distance;
+            }
+            else
+            {
+                return -1;
+            } 
+        }
+        else
+        {
+            Debug.DrawRay(currentPosition, Vector3.down * 500, Color.green, 0.5f);
+            // f(x,y) = (x2 + y - 11)2 + (x + y2 - 7)2 [-6,6]
+            float x = currentPosition.x;
+            float y = currentPosition.z;
+            float x2 = Mathf.Pow(x, 2);
+            float y2 = Mathf.Pow(y, 2);
+            float functionValue = Mathf.Pow((x2 + y - 11), 2) + Mathf.Pow((x + y2 - 7), 2);
+            return functionValue;
+        }
+    }
+}
